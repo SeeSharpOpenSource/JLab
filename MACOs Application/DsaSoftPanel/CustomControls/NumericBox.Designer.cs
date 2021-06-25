34684e9d1bef1ddad8500e204e256318dcd694e8
tao.jing@jytek.com <tao.jing@jytek.com>
2021-06-25 11:51:32 +0800

    提交自定义的数据输入文本框库

---
+++ MACOs Application/DsaSoftPanel/CustomControls/NumericBox.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace CustomControls
+{
+    partial class NumericBox
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox_value = new System.Windows.Forms.TextBox();
+            this.button_up = new System.Windows.Forms.Button();
+            this.button_down = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox_value
+            // 
+            this.textBox_value.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox_value.Location = new System.Drawing.Point(1, 1);
+            this.textBox_value.Name = "textBox_value";
+            this.textBox_value.Size = new System.Drawing.Size(77, 21);
+            this.textBox_value.TabIndex = 1;
+            this.textBox_value.Text = "0";
+            this.textBox_value.SizeChanged += new System.EventHandler(this.textBox_value_SizeChanged);
+            this.textBox_value.TextChanged += new System.EventHandler(this.textBox_value_TextChanged);
+            // 
+            // button_up
+            // 
+            this.button_up.BackColor = System.Drawing.Color.Transparent;
+            this.button_up.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.button_up.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_up.Location = new System.Drawing.Point(79, 1);
+            this.button_up.Name = "button_up";
+            this.button_up.Size = new System.Drawing.Size(20, 10);
+            this.button_up.TabIndex = 2;
+            this.button_up.UseVisualStyleBackColor = false;
+            this.button_up.Click += new System.EventHandler(this.button_up_Click);
+            this.button_up.Paint += new System.Windows.Forms.PaintEventHandler(this.button_up_Paint);
+            // 
+            // button_down
+            // 
+            this.button_down.BackColor = System.Drawing.Color.Transparent;
+            this.button_down.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.button_down.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_down.Location = new System.Drawing.Point(79, 12);
+            this.button_down.Name = "button_down";
+            this.button_down.Size = new System.Drawing.Size(20, 10);
+            this.button_down.TabIndex = 3;
+            this.button_down.UseVisualStyleBackColor = false;
+            this.button_down.Click += new System.EventHandler(this.button_down_Click);
+            this.button_down.Paint += new System.Windows.Forms.PaintEventHandler(this.button_down_Paint);
+            // 
+            // NumericBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button_down);
+            this.Controls.Add(this.button_up);
+            this.Controls.Add(this.textBox_value);
+            this.Name = "NumericBox";
+            this.Size = new System.Drawing.Size(100, 23);
+            this.BackColorChanged += new System.EventHandler(this.NumericBox_BackColorChanged);
+            this.ForeColorChanged += new System.EventHandler(this.NumericBox_ForeColorChanged);
+            this.SizeChanged += new System.EventHandler(this.NumericBox_SizeChanged);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox_value;
+        private System.Windows.Forms.Button button_up;
+        private System.Windows.Forms.Button button_down;
+    }
+}
---
+++ MACOs Application/DsaSoftPanel/CustomControls/NumericBox.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace CustomControls
+{
+    public partial class NumericBox : UserControl
+    {
+        private bool _innerOperation;
+        private const int _heightOffset = 2;
+        private const int _widthOffset = 1;
+
+        private SolidBrush _buttonBrush;
+        private Point[] _trianglePoints = new Point[3];
+
+        public NumericBox()
+        {
+            _innerOperation = true;
+            InitializeComponent();
+            _innerOperation = false;
+            this._innerOperation = false;
+            this._buttonBrush = new SolidBrush(Color.DimGray);
+            for (int i = 0; i < _trianglePoints.Length; i++)
+            {
+                _trianglePoints[i] = new Point();
+            }
+            this._value = 0;
+            this._decimalPlace = 2;
+            this._minimum = 0;
+            this._maximum = 1000;
+            this._textFormat = "F2";
+            this.Increment = 1;
+            ShowValue();
+        }
+
+        public Font TextFont
+        {
+            get { return textBox_value.Font; }
+            set { textBox_value.Font = value; }
+        }
+
+        public Color TriangleColor
+        {
+            get { return _buttonBrush.Color; }
+            set
+            {
+                if (value == _buttonBrush.Color)
+                {
+                    return;
+                }
+                _buttonBrush.Color = value;
+                button_up.Invalidate();
+                button_down.Invalidate();
+            }
+        }
+
+        public Color ButtonBorderColor
+        {
+            get { return button_up.FlatAppearance.BorderColor; }
+            set
+            {
+                button_up.FlatAppearance.BorderColor = value; 
+                button_down.FlatAppearance.BorderColor = value; 
+            }
+        }
+
+        private double _value;
+        public double Value
+        {
+            get { return _value; }
+            set
+            {
+                if (value < _minimum || value > _maximum)
+                {
+                    return;
+                }
+                this._value = value;
+                ShowValue();
+            }
+        }
+
+        private double _maximum;
+
+        public double Maximum
+        {
+            get { return _maximum; }
+            set
+            {
+                if (value <= _minimum)
+                {
+                    return;
+                }
+                this._maximum = value;
+                if (Value > _maximum)
+                {
+                    this.Value = _maximum;
+                }
+            }
+        }
+
+        private double _minimum;
+
+        public double Minimum
+        {
+            get { return _minimum; }
+            set
+            {
+                if (value >= Maximum)
+                {
+                    return;
+                }
+                this._minimum = value;
+                if (Value < _minimum)
+                {
+                    this.Value = _minimum;
+                }
+            }
+        }
+
+        public double Increment { get; set; }
+
+        private string _textFormat;
+        private int _decimalPlace;
+        public int DecimalPlace
+        {
+            get { return _decimalPlace; }
+            set
+            {
+                if (value == _decimalPlace)
+                {
+                    return;
+                }
+                this._decimalPlace = value;
+                _textFormat = $"F{_decimalPlace}";
+            }
+        }
+
+        public HorizontalAlignment TextAlign
+        {
+            get { return textBox_value.TextAlign; }
+            set { textBox_value.TextAlign = value; }
+        }
+        
+        private void NumericBox_ForeColorChanged(object sender, EventArgs e)
+        {
+            this.textBox_value.ForeColor = this.ForeColor;
+        }
+
+        private void NumericBox_SizeChanged(object sender, EventArgs e)
+        {
+            RefreshControlSize();
+        }
+
+        private void textBox_value_SizeChanged(object sender, EventArgs e)
+        {
+            RefreshControlSize();
+        }
+
+        private void RefreshControlSize()
+        {
+            if (_innerOperation)
+            {
+                return;
+            }
+            if (this.Height != textBox_value.Height + _heightOffset)
+            {
+                _innerOperation = true;
+                this.Height = textBox_value.Height + _heightOffset;
+                _innerOperation = false;
+            }
+
+            int extraSpace = (1 == this.Height%2) ? 3 : 2;
+            int buttonHeight = (this.Height - extraSpace) /2;
+            int buttonWidth = buttonHeight*2;
+            int buttonLocationX = this.Width - _widthOffset - buttonWidth;
+            int buttonDownY = (1 == (this.Height - _heightOffset)%2)
+                ? 2 + buttonHeight
+                : 1 + buttonHeight;
+
+            _innerOperation = true;
+
+            textBox_value.Width = this.Width - 3*_widthOffset - buttonWidth;
+            button_up.Location = new Point(buttonLocationX, 1);
+            button_down.Location = new Point(buttonLocationX, buttonDownY);
+            button_up.Width = buttonWidth;
+            button_up.Height = buttonHeight;
+            button_down.Width = buttonWidth;
+            button_down.Height = buttonHeight;
+
+            _innerOperation = false;
+        }
+
+        private void button_up_Paint(object sender, PaintEventArgs e)
+        {
+            const int yOffset = 3;
+            int startY = yOffset - 1;
+            int endY = button_up.Height - yOffset;
+
+            int xOffset = endY - startY;
+
+            int middleX = button_up.Width / 2;
+            int startX = middleX - xOffset;
+            int endX = middleX + xOffset;
+
+            _trianglePoints[0].X = middleX;
+            _trianglePoints[0].Y = startY;
+
+            _trianglePoints[1].X = startX;
+            _trianglePoints[1].Y = endY;
+
+            _trianglePoints[2].X = endX;
+            _trianglePoints[2].Y = endY;
+
+            PlotTriangle(e.Graphics);
+        }
+
+        private void button_down_Paint(object sender, PaintEventArgs e)
+        {
+            const int yOffset = 3;
+            int startY = yOffset;
+            int endY = button_up.Height - yOffset;
+
+            int xOffset = endY - startY;
+
+            int middleX = button_down.Width / 2;
+            int startX = middleX - xOffset;
+            int endX = middleX + xOffset;
+
+            _trianglePoints[0].X = startX;
+            _trianglePoints[0].Y = startY;
+
+            _trianglePoints[1].X = endX;
+            _trianglePoints[1].Y = startY;
+
+            _trianglePoints[2].X = middleX;
+            _trianglePoints[2].Y = endY;
+
+            PlotTriangle(e.Graphics);
+        }
+
+        private void PlotTriangle(Graphics graphics)
+        {
+            graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            graphics.FillPolygon(_buttonBrush, _trianglePoints, FillMode.Winding);
+        }
+
+        private void NumericBox_BackColorChanged(object sender, EventArgs e)
+        {
+            this.textBox_value.BackColor = this.BackColor;
+            this.button_up.BackColor = this.BackColor;
+            this.button_down.BackColor = this.BackColor;
+        }
+
+        private void button_up_Click(object sender, EventArgs e)
+        {
+            this.Value += Increment;
+        }
+
+        private void button_down_Click(object sender, EventArgs e)
+        {
+            this.Value -= Increment;
+        }
+
+        private void textBox_value_TextChanged(object sender, EventArgs e)
+        {
+            if (_innerOperation)
+            {
+                return;
+            }
+            double value;
+            if (double.TryParse(textBox_value.Text, out value))
+            {
+                this._value = value;
+            }
+            ShowValue();
+        }
+
+        private void ShowValue()
+        {
+            _innerOperation = true;
+            this.textBox_value.Text = _value.ToString(_textFormat);
+            _value = double.Parse(this.textBox_value.Text);
+            _innerOperation = false;
+        }
+    }
+}
